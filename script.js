80f6a56d3d2a4303474aa319a62200be799e58d1
Jon <lundbyjon@hotmail.com>
2023-04-22 09:48:41 +0200

    postal code fetch from dataforsyningen and placeholder autofill for "city"

--- script.js
+++ script.js
@@ -3,5 +3,37 @@
 window.addEventListener("load", startApp);
 
 function startApp() {
-    console.log("app is running...");
+  //console.log("app is running...");
+
+  document.querySelector("#address-form").addEventListener("submit", submitClicked);
+}
+
+function submitClicked(event) {
+  console.log("Current target:");
+  console.log(event.currentTarget);
+
+  event.preventDefault();
+
+  const form = {
+    fullname: document.querySelector("#address-form").elements.namedItem("fullname").value,
+    streetname: document.querySelector("#address-form").elements.namedItem("streetname").value,
+    housenumber: document.querySelector("#address-form").elements.namedItem("housenumber").value,
+    postalcode: document.querySelector("#address-form").elements.namedItem("postalcode").value,
+    city: document.querySelector("#address-form").elements.namedItem("city").value,
+    altaddress: document.querySelector("#address-form").elements.namedItem("altaddress").value,
+    phone: document.querySelector("#address-form").elements.namedItem("phone").value,
+    email: document.querySelector("#address-form").elements.namedItem("email").value,
+  };
+
+  console.log(form);
+}
+
+async function getCityInfo() {
+  let postalCode = document.getElementById("postalcode").value;
+
+  const response = await fetch(`https://api.dataforsyningen.dk/postnumre/${postalCode}`);
+  const data = await response.json();
+
+  document.getElementById("city").value = "a city?";
+  console.log(data);
 }
